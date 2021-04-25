af03638da2691e3dd87264a272fba503cefe188c
odysseygthb <prystupkobohdan@gmail.com>
2021-04-25 12:30:26 +0300

    New simple grid

--- src/components/archSimpleGrids.vue
+++ src/components/archSimpleGrids.vue
@@ -71,6 +71,23 @@
     <div class="grid__w300_item"></div>
     <div class="grid__w300_item"></div>
   </section>
+  <section id="grid__seven" class="grid__seven container-y">
+    <div class="grid__seven_item grid_seven_inner">
+      <div class="seven__inner_main"></div>
+      <div class="seven__inner_item"></div>
+      <div class="seven__inner_item"></div>
+      <div class="seven__inner_item"></div>
+      <div class="seven__inner_item"></div>
+    </div>
+    <div class="grid__seven_item"></div>
+    <div class="grid__seven_item grid_seven_inner">
+      <div class="seven__inner_main"></div>
+      <div class="seven__inner_item"></div>
+      <div class="seven__inner_item"></div>
+      <div class="seven__inner_item"></div>
+      <div class="seven__inner_item"></div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -140,6 +157,37 @@ export default {
       justify-content: center;
     }
 
+    .grid__seven {
+      display: grid;
+      grid-template-columns: 1fr;
+      grid-template-rows: repeat(3, 440px);
+      grid-auto-rows: 440px;
+      grid-gap: 50px;
+      justify-content: center;
+    }
+
+    .grid__seven_item {
+      width: 100%;
+    }
+
+    .grid_seven_inner {
+      grid-row: span 2;
+      display: grid;
+      grid-template-columns: 1fr;
+      background-color: transparent;
+      align-content: space-between;
+    }
+
+    .seven__inner_main {
+      background-color: gray;
+      height: 440px;
+    }
+
+    .seven__inner_item {
+      background-color: gray;
+      height: 70px;
+    }
+
     .d-none {
       display: none;
     }
@@ -201,6 +249,21 @@ export default {
     .grid__six {
       grid-template-columns: repeat(6, 1fr);
       grid-auto-rows: 400px;
+    }
+
+    .grid__seven {
+      grid-template-columns: repeat(3, 1fr);
+      grid-template-rows: repeat(2, 440px);
+      grid-auto-rows: auto;
+    }
+
+    .grid__seven_item:not(.grid_seven_inner) {
+      grid-column: 2;
+      grid-row: 2;
+    }
+
+    .grid__seven_item:last-child {
+      grid-column: 3;
     }
   }
 </style>
